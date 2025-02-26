e74efe28bcd6156b987eb577a72e6ba521e3140e
thulanigamtee <thulanigamtee@gmail.com>
2025-02-26 10:20:15 +0200

    rename song model

--- src/app/shared/components/add-song-form/add-song-model.ts
+++ src/app/shared/components/add-song-form/add-song-model.ts
@@ -1,8 +1,8 @@
 import { FormControl } from '@angular/forms';
 
-export interface Song {
+export type Song = {
   title: FormControl<string>;
   author: FormControl<string>;
   thumbnail: FormControl<File | null>;
   audio: FormControl<File | null>;
-}
+};
