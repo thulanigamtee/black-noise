b55ce8b53eaf93c900491ed0a7afd7aef372c29b
thulanigamtee <thulanigamtee@gmail.com>
2025-02-26 10:22:20 +0200

    import add-song-form component

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
-
+import { HlmTooltipTriggerDirective } from '@spartan-ng/ui-tooltip-helm';
+import {
+  BrnDialogContentDirective,
+  BrnDialogTriggerDirective,
+} from '@spartan-ng/brain/dialog';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import { AddSongFormComponent } from '../../shared/components/add-song-form/add-song-form.component';
 @Component({
   selector: 'app-sidebar',
-  imports: [HlmIconDirective, NgIcon, RouterLink, RouterLinkActive],
+  imports: [
+    HlmIconDirective,
+    NgIcon,
+    RouterLink,
+    RouterLinkActive,
+    HlmButtonDirective,
+    HlmTooltipTriggerDirective,
+    BrnDialogTriggerDirective,
+    BrnDialogContentDirective,
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogTitleDirective,
+    HlmButtonDirective,
+    AddSongFormComponent,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
