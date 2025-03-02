56f147e20ac7cd4cfb583d9d049ccb03a8458af2
thulanigamtee <thulanigamtee@gmail.com>
2025-03-02 20:16:13 +0200

    change "author" to "artist"

--- src/app/shared/components/add-song-form/add-song-model.ts
+++ src/app/shared/components/add-song-form/add-song-model.ts
@@ -2,7 +2,7 @@ import { FormControl } from '@angular/forms';
 
 export type Song = {
   title: FormControl<string>;
-  author: FormControl<string>;
+  artist: FormControl<string>;
   thumbnail: FormControl<File | null>;
   audio: FormControl<File | null>;
 };
