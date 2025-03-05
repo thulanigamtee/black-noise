6d9052123cec7ac888859abead0e4562f0ace8b6
thulanigamtee <thulanigamtee@gmail.com>
2025-03-05 04:51:56 +0200

    create songs-list component

---
+++ src/app/shared/components/songs-list/songs-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, inject } from '@angular/core';
+import { AudioPlayerService } from '../../../services/audio-player.service';
+import { AppwriteService } from '../../../services/appwrite.service';
+import { Song } from '../../models/song.model';
+import { AsyncPipe, NgStyle } from '@angular/common';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { NgIcon } from '@ng-icons/core';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+
+@Component({
+  selector: 'app-songs-list',
+  imports: [AsyncPipe, HlmButtonDirective, NgIcon, HlmIconDirective, NgStyle],
+  templateUrl: './songs-list.component.html',
+  styleUrl: './songs-list.component.scss',
+})
+export class SongsListComponent {
+  private audioPlayerService = inject(AudioPlayerService);
+  private appwriteService = inject(AppwriteService);
+
+  songs$ = this.appwriteService.fetchSongs();
+  currentSong$ = this.audioPlayerService.currentSong$;
+  isPlaying$ = this.audioPlayerService.isPlaying$;
+
+  currentSong: Song | null = null;
+  isPlaying = false;
+
+  constructor() {
+    this.currentSong$.subscribe((song) => {
+      this.currentSong = song;
+    });
+    this.isPlaying$.subscribe((value) => {
+      this.isPlaying = value;
+    });
+  }
+
+  playSong(song: Song) {
+    this.audioPlayerService.playSong(song);
+  }
+
+  pause() {
+    this.audioPlayerService.pause();
+  }
+
+  togglePlay(song: Song) {
+    if (song === this.currentSong && this.isPlaying) {
+      this.pause();
+    } else this.playSong(song);
+  }
+}
