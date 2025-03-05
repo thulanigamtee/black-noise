286d38dc699eb5adad7442f696924487cb879005
thulanigamtee <thulanigamtee@gmail.com>
2025-03-05 04:51:15 +0200

    implement functionality to mute volume

--- src/app/services/audio-player.service.ts
+++ src/app/services/audio-player.service.ts
@@ -5,17 +5,19 @@ import { Song } from '../shared/models/song.model';
 @Injectable({ providedIn: 'root' })
 export class AudioPlayerService {
   private audio = new Audio();
+
   private currentSongSubject = new BehaviorSubject<Song | null>(null);
   private isPlayingSubject = new BehaviorSubject<boolean>(false);
   private currentTimeSubject = new BehaviorSubject<number>(0);
   private durationSubject = new BehaviorSubject<number>(0);
-  private volumeSubject = new BehaviorSubject<number>(1);
 
   currentSong$ = this.currentSongSubject.asObservable();
   isPlaying$ = this.isPlayingSubject.asObservable();
   currentTime$ = this.currentTimeSubject.asObservable();
   duration$ = this.durationSubject.asObservable();
-  volume$ = this.volumeSubject.asObservable();
+
+  private currentVolume = 1;
+  volume$ = new BehaviorSubject<number>(this.currentVolume);
 
   constructor() {
     this.audio.addEventListener('timeupdate', () =>
@@ -25,6 +27,25 @@ export class AudioPlayerService {
     this.audio.addEventListener('loadedmetadata', () =>
       this.durationSubject.next(this.audio.duration)
     );
+
+    this.audio.volume = this.currentVolume;
+  }
+
+  setVolume(event: Event) {
+    const volume = (event.target as HTMLInputElement).valueAsNumber;
+    this.currentVolume = volume;
+    this.audio.volume = volume;
+    this.volume$.next(volume);
+  }
+
+  toggleMute() {
+    if (this.audio.volume > 0) {
+      this.currentVolume = this.audio.volume;
+      this.audio.volume = 0;
+    } else {
+      this.audio.volume = this.currentVolume;
+    }
+    this.volume$.next(this.audio.volume);
   }
 
   playSong(song: Song) {
@@ -43,10 +64,5 @@ export class AudioPlayerService {
 
   seek(time: number) {
     this.audio.currentTime = time;
-  }
-
-  setVolume(volume: number) {
-    this.audio.volume = volume;
-    this.volumeSubject.next(volume);
   }
 }
