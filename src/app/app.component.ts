3ee21c633606b4bd0c1ed5460ee4bebd1e2a12cc
thulanigamtee <thulanigamtee@gmail.com>
2025-03-05 04:48:53 +0200

    import player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { PlayerComponent } from './shared/components/player/player.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, HeaderComponent, RouterOutlet],
+  imports: [SidebarComponent, HeaderComponent, RouterOutlet, PlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
