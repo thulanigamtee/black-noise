04bec3a92f17f663966af4acb71f14cdcb5c9db6
thulanigamtee <thulanigamtee@gmail.com>
2025-03-08 02:11:53 +0200

    implement toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { PlayerComponent } from './shared/components/player/player.component';
+import { ToastComponent } from './shared/components/toast/toast.component';
+import { ToastService } from './services/toast.service';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, HeaderComponent, RouterOutlet, PlayerComponent],
+  imports: [
+    SidebarComponent,
+    HeaderComponent,
+    RouterOutlet,
+    PlayerComponent,
+    ToastComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  toastService = inject(ToastService);
+
+  showSuccess() {
+    this.toastService.showToast('Operation successful!', 'success');
+  }
+
+  showError() {
+    this.toastService.showToast('Something went wrong!', 'error');
+  }
+}
