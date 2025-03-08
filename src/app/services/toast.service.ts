af862c111f6f4683b7c1f283926a2054b0e1f5d6
thulanigamtee <thulanigamtee@gmail.com>
2025-03-08 02:19:36 +0200

    add toast functionality

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+
+export interface ToastMessage {
+  id: number;
+  message: string;
+  type: 'success' | 'error' | 'warning' | 'info';
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToastService {
+  private nextId = 0;
+  toasts = signal<ToastMessage[]>([]);
 
-  constructor() { }
+  showToast(
+    message: string,
+    type: 'success' | 'error' | 'warning' | 'info',
+    duration = 3000
+  ) {
+    const id = this.nextId++;
+    this.toasts.update((toasts) => [...toasts, { id, message, type }]);
+
+    setTimeout(() => this.removeToast(id), duration);
+  }
+
+  removeToast(id: number) {
+    this.toasts.update((toasts) => toasts.filter((toast) => toast.id !== id));
+  }
 }
