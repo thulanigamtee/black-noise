83d4f5aa6c27848da3668f44d33dd140d423a071
thulanigamtee <thulanigamtee@gmail.com>
2025-03-08 02:18:23 +0200

    create current song variable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme/theme.service';
+import { SongsListComponent } from '../../shared/components/songs-list/songs-list.component';
+import { AudioPlayerService } from '../../services/audio-player.service';
+import { AsyncPipe, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SongsListComponent, NgStyle, AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  constructor(private themeService: ThemeService) {}
+  private themeService = inject(ThemeService);
+  private audioPlayerService = inject(AudioPlayerService);
+
+  currentSong$ = this.audioPlayerService.currentSong$;
 
   changeTheme(theme: 'light' | 'dark' | 'system') {
     this.themeService.Theme = theme;
