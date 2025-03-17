191e851461ef7444e90709d1c820e1c644b1954c
thulanigamtee <thulanigamtee@gmail.com>
2025-03-17 18:41:21 +0200

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ExploreComponent } from './pages/explore/explore.component';
-import { LibraryComponent } from './pages/library/library.component';
+import { AddSongComponent } from './pages/add-song/add-song.component';
 
 export const routes: Routes = [
   { component: HomeComponent, path: '' },
-  { component: ExploreComponent, path: 'explore' },
-  { component: LibraryComponent, path: 'library' },
+  { component: AddSongComponent, path: 'addSong' },
 ];
