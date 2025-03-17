f9a5a0b5015357ef1a27d04ad9a00ad4dd630b78
thulanigamtee <thulanigamtee@gmail.com>
2025-03-17 17:59:22 +0200

    install alert dialog ui component

---
+++ src/app/shared/ui/ui-alertdialog-helm/src/lib/hlm-alert-dialog.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, forwardRef, ViewEncapsulation } from '@angular/core';
+import { BrnAlertDialogComponent, BrnAlertDialogOverlayComponent } from '@spartan-ng/brain/alert-dialog';
+import { BrnDialogComponent, provideBrnDialogDefaultOptions } from '@spartan-ng/brain/dialog';
+import { HlmAlertDialogOverlayDirective } from './hlm-alert-dialog-overlay.directive';
+
+@Component({
+	selector: 'hlm-alert-dialog',
+	standalone: true,
+	template: `
+		<brn-alert-dialog-overlay hlm />
+		<ng-content />
+	`,
+	providers: [
+		{
+			provide: BrnDialogComponent,
+			useExisting: forwardRef(() => HlmAlertDialogComponent),
+		},
+		provideBrnDialogDefaultOptions({
+			closeDelay: 100,
+		}),
+	],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
+	exportAs: 'hlmAlertDialog',
+	imports: [BrnAlertDialogOverlayComponent, HlmAlertDialogOverlayDirective],
+})
+export class HlmAlertDialogComponent extends BrnAlertDialogComponent {}
