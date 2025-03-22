34c361f2b3c345021d3cb1216ac9c5e22ec1258c
thulanigamtee <thulanigamtee@gmail.com>
2025-03-22 02:27:23 +0200

    update sidebar component

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,41 +1,12 @@
-import { Component, inject, viewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
-import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
-import { HlmTooltipTriggerDirective } from '@spartan-ng/ui-tooltip-helm';
-import {
-  BrnDialogComponent,
-  BrnDialogContentDirective,
-  BrnDialogTriggerDirective,
-} from '@spartan-ng/brain/dialog';
-import {
-  HlmDialogComponent,
-  HlmDialogContentComponent,
-  HlmDialogHeaderComponent,
-  HlmDialogTitleDirective,
-} from '@spartan-ng/ui-dialog-helm';
-import { AddSongFormComponent } from '../../shared/components/add-song-form/add-song-form.component';
 import { AuthService } from '../../services/auth/auth.service';
 import { UserResource } from '@clerk/types';
 @Component({
   selector: 'app-sidebar',
-  imports: [
-    HlmIconDirective,
-    NgIcon,
-    RouterLink,
-    RouterLinkActive,
-    HlmButtonDirective,
-    HlmTooltipTriggerDirective,
-    BrnDialogTriggerDirective,
-    BrnDialogContentDirective,
-    HlmDialogComponent,
-    HlmDialogContentComponent,
-    HlmDialogHeaderComponent,
-    HlmDialogTitleDirective,
-    HlmButtonDirective,
-    AddSongFormComponent,
-  ],
+  imports: [HlmIconDirective, NgIcon, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
@@ -49,13 +20,6 @@ export class SidebarComponent {
 
   routes: { name: string; icon: string; path: string }[] = [
     { name: 'home', icon: 'lucideHouse', path: '' },
-    { name: 'explore', icon: 'lucideCompass', path: 'explore' },
-    { name: 'library', icon: 'lucideLibrary', path: 'library' },
+    { name: 'add song', icon: 'lucidePlus', path: 'addSong' },
   ];
-
-  public viewchildDialogRef = viewChild(BrnDialogComponent);
-
-  closeDialog() {
-    this.viewchildDialogRef()?.close({});
-  }
 }
