f5f95b6340fd420cdecd5490d6ee0d11d0f8c40e
thulanigamtee <thulanigamtee@gmail.com>
2025-03-22 02:28:31 +0200

    update songs-list

--- src/app/shared/components/songs-list/songs-list.component.ts
+++ src/app/shared/components/songs-list/songs-list.component.ts
@@ -2,21 +2,14 @@ import { Component, inject } from '@angular/core';
 import { AudioPlayerService } from '../../../services/audio-player.service';
 import { AppwriteService } from '../../../services/appwrite.service';
 import { Song } from '../../models/song.model';
-import { AsyncPipe, NgClass, NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { NgIcon } from '@ng-icons/core';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-songs-list',
-  imports: [
-    AsyncPipe,
-    HlmButtonDirective,
-    NgIcon,
-    HlmIconDirective,
-    NgStyle,
-    NgClass,
-  ],
+  imports: [HlmButtonDirective, NgIcon, HlmIconDirective, NgStyle, NgClass],
   templateUrl: './songs-list.component.html',
   styleUrl: './songs-list.component.scss',
 })
@@ -31,12 +24,14 @@ export class SongsListComponent {
   currentSong: Song | null = null;
   isPlaying: boolean = false;
 
+  songs: Song[] = [];
   constructor() {
     this.currentSong$.subscribe((song) => (this.currentSong = song));
     this.isPlaying$.subscribe((isPlaying) => (this.isPlaying = isPlaying));
     this.songs$.subscribe((songs) =>
       this.audioPlayerService.setPlaylist(songs)
     );
+    this.songs$.subscribe((songs) => (this.songs = songs));
   }
 
   togglePlay(song: Song) {
