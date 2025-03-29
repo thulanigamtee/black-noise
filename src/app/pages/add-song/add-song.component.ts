42718a9d9f5da79198f4baf00e7828704edd3156
thulanigamtee <thulanigamtee@gmail.com>
2025-03-29 04:00:35 +0200

    update add-song height

--- src/app/pages/add-song/add-song.component.ts
+++ src/app/pages/add-song/add-song.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AddSongFormComponent } from '../../shared/components/add-song-form/add-song-form.component';
+import { AudioPlayerService } from '../../services/audio-player.service';
+import { AsyncPipe, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-add-song',
-  imports: [AddSongFormComponent],
+  imports: [AddSongFormComponent, NgStyle, AsyncPipe],
   templateUrl: './add-song.component.html',
   styleUrl: './add-song.component.scss',
 })
-export class AddSongComponent {}
+export class AddSongComponent {
+  audioPlayerService = inject(AudioPlayerService);
+  currentSong$ = this.audioPlayerService.currentSong$;
+}
